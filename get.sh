bfc70723e091e7f6dfbf6c6a0665316eb480da2b
Gocho Mugo <mugo@forfuture.co.ke>
2015-04-19 14:17:40 +0300

    fix error #1

--- get.sh
+++ get.sh
@@ -6,18 +6,17 @@
 #
 
 
-DIRNAME=moz-sdk
+CMDNAME=moz-sdk
 
 
 echo "⇒ cloning the installer's repo"
-git clone https://github.com/GochoMugo/moz-addon-sdk-installer.git $DIRNAME
-
-
-echo "⇒ moving installer to /usr/local/lib (requires  \"sudo\" powers)"
-sudo rm -rf /usr/local/lib/$DIRNAME
-sudo mv $DIRNAME /usr/local/lib
+git clone https://github.com/GochoMugo/moz-addon-sdk-installer.git $CMDNAME
 
 
 echo "⇒ adding the command (requires  \"sudo\" powers): moz-sdk"
-sudo ln -sf /usr/local/lib/$DIRNAME/installer.sh /usr/local/bin/moz-sdk
+sudo mv $CMDNAME/installer.sh /usr/local/bin/moz-sdk
+
+
+echo "⇒ cleaning up"
+rm -rf $CMDNAME
 
